0cacbfdd50c92ed1d934962f7ba9f54ea920f9b3
tesfahunBirega <tesfubirega@gmail.com>
2024-04-08 15:45:26 +0300

    first commit

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -2,31 +2,26 @@ const path = require('path');
 const configs = require('./config');
 const { User, UserProfile, userAdmin, Event, FeedBack, Donation, Post, Voluntery } = require('../models/index');
 
-// const userProfile = require('../models/userProfile.model.js');
-// const userAdmin = require('../models/userAdmin.model.js');
-
-// configuration file for TypeORM db connection
-
 module.exports = {
-  type: 'postgres', // Change the type to mysql
-  host: configs.postgres.host, // Use MySQL host configuration
-  port: configs.postgres.port, // Use MySQL port configuration
-  username: configs.postgres.userName, // Use MySQL username configuration
-  password: configs.postgres.password, // Use MySQL password configuration
-  database: configs.postgres.database, // Use MySQL database configuration
-  // entities: [__dirname + "/../models/*.js"],
+  type: 'postgres',
+  host: configs.postgres.host,
+  port: configs.postgres.port,
+  username: configs.postgres.userName,
+  password: configs.postgres.password,
+  database: configs.postgres.database,
   entities: [User, UserProfile, userAdmin, Event, FeedBack, Donation, Post, Voluntery],
-
   synchronize: true,
-  // migrationsRun: true,
-  migrations: [path.join(__dirname, '../migrations/*.js')], // Path to migration files
+  migrations: [path.join(__dirname, '../migrations/*.js')],
   cli: {
     entitiesDir: path.join(__dirname, '../models'),
     migrationsDir: path.join(__dirname, '../migrations*.js'),
   },
   extra: {
-    connectionLimit: configs.postgres.maxConn, // Set the pool size to MySQL configuration
+    connectionLimit: configs.postgres.maxConn,
     idleTimeoutMillis: configs.postgres.idleTimeOut,
     connectionTimeoutMillis: configs.postgres.connTimeOut,
+    ssl: {
+      rejectUnauthorized: false, // Disable SSL certificate validation
+    },
   },
 };
