d4f45d3212cad76c4d5d1c3ab6c7b14965f72014
tesfahunBirega <tesfubirega@gmail.com>
2024-04-08 20:49:23 +0300

    update the charity

--- src/config/paymentConfig.js
+++ src/config/paymentConfig.js
@@ -1,4 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 module.exports = {
-  stripeApiKey:
-    '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',
+  stripeApiKey: process.env.STRIPE_API_KEY,
 };
